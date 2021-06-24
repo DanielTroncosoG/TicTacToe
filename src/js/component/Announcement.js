1597f235a5a2d09a351c8dc09f6fd2ea7f9d73d2
DanielTroncosoG <di.trgonzalez@gmail.com>
2021-06-24 20:57:20 +0000

    falta poco commit

--- src/js/component/Announcement.js
+++ src/js/component/Announcement.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
+import "/workspace/TicTacToe/src/styles/Announcement.scss";
+import PropTypes from "prop-types";
 
 export default class Announcement extends Component {
 	render() {
 		return (
-			<div className="visible">
+			<div className={this.props.winner ? "visible" : "hidden"}>
 				<h2>Winner</h2>
 			</div>
 		);
 	}
 }
+Announcement.propTypes = {
+	winner: PropTypes.any
+};
--- src/js/component/App.js
+++ src/js/component/App.js
@@ -10,17 +10,101 @@ class App extends Component {
 		super();
 		this.state = {
 			gameBoard: [" ", " ", " ", " ", " ", " ", " ", " ", " "],
-			turn: "x"
+			turn: "x",
+			winner: null
 		};
 	}
-	updateBoard(loc, player) {}
+	updateBoard(loc, player) {
+		if (
+			this.state.gameBoard[loc] === "x" ||
+			this.state.gameBoard[loc] === "o"
+		) {
+			return;
+		}
+		let currentGameboard = this.state.gameBoard;
+		currentGameboard.splice(loc, 1, this.state.turn);
+		this.setState({ gameBoard: currentGameboard });
+
+		let topRow =
+			this.state.gameBoard[0] +
+			this.state.gameBoard[1] +
+			this.state.gameBoard[2];
+		if (topRow.match(/xxx|ooo/)) {
+			this.setState(this.state.turn);
+			return;
+		}
+		let middleRow =
+			this.state.gameBoard[3] +
+			this.state.gameBoard[4] +
+			this.state.gameBoard[5];
+		if (middleRow.match(/xxx|ooo/)) {
+			this.setState(this.state.turn);
+			return;
+		}
+		let bottomRow =
+			this.state.gameBoard[6] +
+			this.state.gameBoard[7] +
+			this.state.gameBoard[8];
+		if (bottomRow.match(/xxx|ooo/)) {
+			this.setState(this.state.turn);
+			return;
+		}
+		let leftCol =
+			this.state.gameBoard[0] +
+			this.state.gameBoard[3] +
+			this.state.gameBoard[6];
+		if (leftCol.match(/xxx|ooo/)) {
+			this.setState(this.state.turn);
+			return;
+		}
+		let middleCol =
+			this.state.gameBoard[1] +
+			this.state.gameBoard[4] +
+			this.state.gameBoard[7];
+		if (middleCol.match(/xxx|ooo/)) {
+			this.setState(this.state.turn);
+			return;
+		}
+		let rigthCol =
+			this.state.gameBoard[2] +
+			this.state.gameBoard[5] +
+			this.state.gameBoard[8];
+		if (rigthCol.match(/xxx|ooo/)) {
+			this.setState(this.state.turn);
+			return;
+		}
+		let leftDiag =
+			this.state.gameBoard[0] +
+			this.state.gameBoard[4] +
+			this.state.gameBoard[8];
+		if (leftDiag.match(/xxx|ooo/)) {
+			this.setState(this.state.turn);
+			return;
+		}
+		let rightDiag =
+			this.state.gameBoard[2] +
+			this.state.gameBoard[4] +
+			this.state.gameBoard[6];
+		if (rightDiag.match(/xxx|ooo/)) {
+			this.setState(this.state.turn);
+			return;
+		}
+	}
+
+	resetBoard() {
+		this.setState({
+			gameBoard: [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+			turn: "x",
+			winner: null
+		});
+	}
 
 	render() {
 		return (
 			<div className="container">
 				<div className="menu">
 					<h1>Tic Tac Toe</h1>
-					<Announcement />
+					<Announcement winner={this.state.winner} />
 					<ResetButton />
 				</div>
 				{this.state.gameBoard.map(
